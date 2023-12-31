a3262d3e21e62a357671a03669d37902a40166fc
ice <ice_cream271@outlook.com>
2023-12-31 15:12:11 +0800

    pca ver0.1

---
+++ src/degree_reduce/pca.py
@@ -0,0 +1,27 @@
+
+import numpy
+from scipy import sparse
+from scipy.sparse.linalg import svds
+
+
+class PCA():
+    def __init__(self) -> None:
+        self.feature_degree = 0                 # feature degree of input data
+        self.trunc_degree = 0                   # feature degree of output data
+        self.projectMatrix = numpy.zeros([0,0]) # project matrix
+        
+
+    # generate projMtrx by training data
+    #   data: N * m sparse matrix, N=samples, m=features
+    #   trunc_degree: desire output feature degree
+    def train_and_proj(self, data:sparse.csr_matrix, trunc_degree):
+        assert data.shape[1] >= trunc_degree
+        ut, st, vt = svds(data, trunc_degree)
+
+        self.feature_degree = data.shape[1]
+        self.trunc_degree = trunc_degree
+        self.projectMatrix = vt
+
+        return ut@numpy.diag(st)
+    def project(self, data):
+        return data @ self.projectMatrix
