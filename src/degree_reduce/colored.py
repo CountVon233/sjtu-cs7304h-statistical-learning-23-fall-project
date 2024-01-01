84f44c7521614d2ce7ee63dbc498c00fd57d5f78
CountVon233 <1084383190@qq.com>
2024-01-01 21:22:51 +0800

    color run

--- src/degree_reduce/colored.py
+++ src/degree_reduce/colored.py
@@ -4,14 +4,19 @@ from scipy import sparse, linalg, stats
 from scipy.sparse.linalg import svds, aslinearoperator, LinearOperator
 from pca import PCA
 from matplotlib import pyplot as plt
+from pathlib import Path
 
+train_relative_path = "..\\..\\dataset\\train_feature.pkl"
+dir = Path(__file__).parent
+train_absolute_path = dir.joinpath(train_relative_path)
 
-path = '/data/Documents/study/2023-Fall/CS7304H-StatisticalLearning/statistical-learning-project/dataset/train_feature.pkl'
-label_path = '/data/Documents/study/2023-Fall/CS7304H-StatisticalLearning/statistical-learning-project/dataset/train_labels.npy'
-with open(path, "rb") as file:
+label_relative_path = "..\\..\\dataset\\train_labels.npy"
+label_absolute_path = dir.joinpath(label_relative_path)
+
+with open(train_absolute_path, "rb") as file:
     TrainFeature = pickle.load(file)
 
-label = np.load(label_path)
+label = np.load(label_absolute_path)
 color = [ plt.get_cmap("seismic", 20)(i) for i in label[:500] ]
 pca = PCA()
 
@@ -22,6 +27,6 @@ plt.set_cmap(plt.get_cmap('seismic', 20))
 im = ax.scatter( feature[:500, 0], feature[:500, 1], feature[:500, 2], s=20, c=color, marker='.' )
 
 plt.show()
-plt.savefig('./src/degree_reduce/colored.png')
+plt.savefig('.\\colored.png')
 
 
