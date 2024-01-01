9078235e6d91552a6252c647a8bf9a0670b80a16
CountVon233 <1084383190@qq.com>
2024-01-02 04:09:04 +0800

    package

--- src/run_LR.py
+++ src/run_LR.py
@@ -4,7 +4,7 @@ import scipy
 from pathlib import Path
 from logistic_regression import LR
 
-def run_lr(train_data, train_label):
+def run_lr(train_data, train_label, test_data, test_label):
     LR_solver = LR.solver(train_data.shape[1], 2)
     LR_solver.train(train_data, train_label, 100)
-    LR_solver.test(train_data, train_label)
+    LR_solver.test(test_data, test_label)
